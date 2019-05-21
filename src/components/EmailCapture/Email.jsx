ed560017cf604e5319496d13a0dab25d8e653dc7
Ryan Luu <ryan@vumble.com>
2019-05-20 20:56:38 -0400

    Working Email Submission to GSheet BUT no Event Trigger for FE

--- src/components/EmailCapture/Email.jsx
+++ src/components/EmailCapture/Email.jsx
@@ -2,24 +2,50 @@ import React from "react";
 import "./email.scss";
 
 export default class Hero extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      submitted: false
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit() {
+    console.log("hi");
+    this.setState({ submitted: true });
+  }
+
   render() {
+    const { submitted } = this.state;
     return (
       <div className="email-container">
         <h4 className="email-cta">Get notified about virtual tours.</h4>
-        <form
-          class="gform"
-          method="POST"
-          data-email="rluu34@gmail.com"
-          action="https://script.google.com/macros/s/AKfycbwCka1fBUp7cvMvAFmV_0LEBIcEvNY5tdC9XjJ5dzX-gWLEQn0/exec"
-        >
-          <input
-            className="email-address"
-            type="text"
-            placeholder="Email Address"
-            name="email"
-          />
-          <input className="submit" type="submit" value="Submit" />
-        </form>
+        {submitted ? (
+          <>
+            <div>Thanks</div>
+          </>
+        ) : (
+          <>
+            <form
+              class="gform"
+              method="POST"
+              data-email="osebo@galleryoffreedom.com"
+              email="osebo@galleryoffreedom.com"
+              action="https://script.google.com/macros/s/AKfycbwCka1fBUp7cvMvAFmV_0LEBIcEvNY5tdC9XjJ5dzX-gWLEQn0/exec"
+              target="hidden-iframe"
+            >
+              <input
+                className="email-address"
+                id="userEmail"
+                type="text"
+                placeholder="Email Address"
+                name="email"
+              />
+              <input className="submit" type="submit" value="Submit" />
+              <iframe name="hidden-iframe" style={{ display: "none" }} />
+            </form>
+          </>
+        )}
       </div>
     );
   }
