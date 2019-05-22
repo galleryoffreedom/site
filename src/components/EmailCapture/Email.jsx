940be27fa17be9c5f4f1f4ca12f50af631d05a41
Ryan Luu <ryan@vumble.com>
2019-05-21 21:22:10 -0400

    Mailchimp Email Triggers new page

--- src/components/EmailCapture/Email.jsx
+++ src/components/EmailCapture/Email.jsx
@@ -1,25 +1,82 @@
 import React from "react";
 import "./email.scss";
 
-export default class Hero extends React.Component {
+export default class Email extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: ""
+    };
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onChange(event) {
+    this.setState({ value: event.target.value });
+  }
   render() {
     return (
       <div className="email-container">
         <h4 className="email-cta">Get notified about virtual tours.</h4>
-        <form
-          class="gform"
-          method="POST"
-          data-email="rluu34@gmail.com"
-          action="https://script.google.com/macros/s/AKfycbwCka1fBUp7cvMvAFmV_0LEBIcEvNY5tdC9XjJ5dzX-gWLEQn0/exec"
-        >
-          <input
-            className="email-address"
-            type="text"
-            placeholder="Email Address"
-            name="email"
-          />
-          <input className="submit" type="submit" value="Submit" />
-        </form>
+        <div id="mc_embed_signup">
+          <form
+            action="https://galleryoffreedom.us20.list-manage.com/subscribe/post?u=2b4c8bfb55ef732df9b7bb6dc&amp;id=ff6c90c473"
+            method="post"
+            id="mc-embedded-subscribe-form"
+            name="mc-embedded-subscribe-form"
+            className="validate"
+            target="_blank"
+          >
+            <div id="mc_embed_signup_scroll">
+              <h2>Subscribe</h2>
+              <div className="indicates-required">
+                <span className="asterisk">*</span> indicates required
+              </div>
+              <div className="mc-field-group">
+                <label htmlFor="mce-EMAIL">
+                  Email Address <span className="asterisk">*</span>
+                </label>
+                <input
+                  type="email"
+                  value=""
+                  name="EMAIL"
+                  className="required email"
+                  id="mce-EMAIL"
+                />
+              </div>
+              <div id="mce-responses" className="clear">
+                <div
+                  className="response"
+                  id="mce-error-response"
+                  style={{ display: "none" }}
+                />
+                <div
+                  className="response"
+                  id="mce-success-response"
+                  style={{ display: "none" }}
+                />
+              </div>
+              <div
+                style={{ position: "absolute", left: "-5000px" }}
+                aria-hidden="true"
+              >
+                <input
+                  type="text"
+                  name="b_2b4c8bfb55ef732df9b7bb6dc_ff6c90c473"
+                  tabIndex="-1"
+                />
+              </div>
+              <div className="clear">
+                <input
+                  type="submit"
+                  value="Subscribe"
+                  name="subscribe"
+                  id="mc-embedded-subscribe"
+                  className="button"
+                />
+              </div>
+            </div>
+          </form>
+        </div>
       </div>
     );
   }
